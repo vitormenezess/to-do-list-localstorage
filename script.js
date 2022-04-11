7ec390a7ed50819f9cb9e86d1f35d9f03cffc302
vitormenezess <vitormenezes43@icloud.com>
2022-04-11 17:18:51 -0300

    11/04

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 "use strict";
 const plus = document.getElementById("plus");
+const txt = document.getElementById("novoItem");
+let banco = [
+  { tarefa: "Estudar", status: "" },
+  { tarefa: "netflix", status: "checked" },
+  { tarefa: "teste", status: "checked" },
+];
 
-const criarItem = () => {
+function criarItem(tarefa, status) {
   //criar elememto no html
   const item = document.createElement("label");
   //add class no elemento
   item.classList.add("todo-item");
-  const novoItem = document.getElementById("novoItem");
-  if (novoItem.value === "") {
-    alert("informe uma tarefa");
-  } else {
-    item.innerHTML = `<input type="checkbox" />
-  <div>${novoItem.value}</div>
+  item.innerHTML = `<input type="checkbox" ${status}/>
+  <div>${tarefa}</div>
   <input
     type="image"
     src="imagens/trash-can-regular.svg"
@@ -21,9 +23,35 @@ const criarItem = () => {
     id="deletar"
   />`;
 
-    document.getElementById("todoList").appendChild(item);
-    novoItem.value = "";
+  document.getElementById("todoList").appendChild(item);
+}
+const limparTarefas = () => {
+  const todoList = document.getElementById("todoList");
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild);
   }
 };
 
-plus.addEventListener("click", criarItem);
+const atualizarTela = () => {
+  limparTarefas();
+  banco.forEach((item) => criarItem(item.tarefa, item.status));
+};
+
+const inserirItem = (evento) => {
+  if (evento.key == "Enter") {
+    banco.push({ tarefa: txt.value, status: "" });
+    atualizarTela();
+    txt.value = "";
+  }
+  if (evento.type == "click") {
+    banco.push({ tarefa: txt.value, status: "" });
+    atualizarTela();
+    txt.value = "";
+  }
+  
+};
+
+txt.addEventListener("keypress", inserirItem);
+plus.addEventListener("click", inserirItem);
+
+atualizarTela();
